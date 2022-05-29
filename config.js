7129a9455910a28c8b7b80cdc9f46c9669b0eb16
cetherdev <robert.hct@gmail.com>
2022-05-29 15:28:02 -0500

    feat: configure mysql database store

--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
 	jwt: {
 		secret: process.env.JWT_SECRET || 'secretKey',
 	},
+	mysql: {
+		host: process.env.MYSQL_HOST || '127.0.0.1',
+		user: process.env.MYSQL_USER || 'root',
+		password: process.env.MYSQL_PASSWORD || 'password',
+		database: process.env.MYSQL_DB || 'example_user_auth',
+	}
 };
---
+++ store/mysql.js
@@ -0,0 +1,113 @@
+const mysql = require('mysql2');
+const config = require('../config');
+
+const dbconf = {
+	host: config.mysql.host,
+	user: config.mysql.user,
+	password: config.mysql.password,
+	database: config.mysql.database,
+};
+
+//Conection
+let connection;
+
+function handleCon() {
+	connection = mysql.createConnection(dbconf);
+	connection.connect((err) => {
+		if (err) {
+			console.error('[db err]', err);
+			setTimeout(handleCon, 2000);
+		}
+		console.log('DB Connected!');
+	});
+
+	connection.on('error', (err) => {
+		console.error('[db err]', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleCon();
+		} else {
+			throw err;
+		}
+	});
+}
+
+handleCon();
+
+function list(table) {
+	return new Promise((resolve, reject) => {
+		connection.query(`SELECT * FROM ${table}`, (err, data) => {
+			if (err) return reject(err);
+			resolve(data);
+		});
+	});
+}
+
+function get(table, id) {
+	return new Promise((resolve, reject) => {
+		connection.query(
+			`SELECT * FROM ${table} WHERE id = '${id}'`,
+			(err, data) => {
+				if (err) return reject(err);
+				resolve(data);
+			}
+		);
+	});
+}
+
+function insert(table, data) {
+	return new Promise((resolve, reject) => {
+		connection.query(`INSERT INTO ${table} SET ?`, data, (err, result) => {
+			if (err) return reject(err);
+			resolve(result);
+		});
+	});
+}
+
+async function remove(table, id) {
+	return true;
+}
+
+async function query(table, q) {
+	let col = await list(table);
+	let keys = Object.keys(q);
+	let key = keys[0];
+
+	return col.filter((item) => item[key] === q[key])[0] || null;
+}
+
+function update(table, data) {
+	return new Promise((resolve, reject) => {
+		connection.query(
+			`UPDATE ${table} SET ? WHERE id=?`,
+			[data, data.id],
+			(err, result) => {
+				if (err) return reject(err);
+				resolve(result);
+			}
+		);
+	});
+}
+
+function upsert(table, data) {
+	if (data && data.id) {
+		return update(table, data);
+	} else {
+		return insert(table, data);
+	}
+}
+
+function query(table, query) {
+	return new Promise((resolve, reject) => {
+		connection.query(`SELECT * FROM ${table} WHERE ?`, query, (err, result) => {
+			if (err) return reject(err);
+			resolve(result[0] || null);
+		});
+	});
+}
+
+module.exports = {
+	list,
+	get,
+	upsert,
+	query,
+};

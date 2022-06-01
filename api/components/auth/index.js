22f7e65ad177205e0432fc16ee7938bf2ff93c17
cetherdev <robert.hct@gmail.com>
2022-05-31 22:32:20 -0500

    feat: configure api for vercel

--- api/components/auth/index.js
+++ api/components/auth/index.js
@@ -1,4 +1,11 @@
 const ctrl = require('./controller');
-const store = require('../../../store/remote-mysql');
+const config = require('../../../config');
+
+let store;
+if (config.remoteDB == false) {
+	store = require('../../../store/mysql');
+} else {
+	store = require('../../../store/remote-mysql');
+}
 
 module.exports = ctrl(store);
--- config.js
+++ config.js
@@ -6,15 +6,16 @@ module.exports = {
 		secret: process.env.JWT_SECRET || 'secretKey',
 	},
 	mysql: {
-		host: process.env.MYSQL_HOST || '127.0.0.1',
-		user: process.env.MYSQL_USER || 'root',
-		password: process.env.MYSQL_PASSWORD || 'password',
+		host: process.env.MYSQL_HOST || '',
+		user: process.env.MYSQL_USER || '',
+		password: process.env.MYSQL_PASSWORD || '',
 		database: process.env.MYSQL_DB || 'example_user_auth',
 	},
 	mysqlService: {
 		host: process.env.MYSQL_SRV_HOST || 'localhost',
 		port: process.env.MYSQL_SRV_PORT || 3001,
 	},
+	remoteDB: process.env.REMOTE_DB || false,
 	post: {
 		port: process.env.POST_PORT || 3002,
 	},
